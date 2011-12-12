3854916ad81a720b47202c057be21ec6f59fcbd2
Catalin Dumitru <catalin.dumitru@live.com>
2011-12-12 23:02:56 +0200

    Prima versiune goala

---
+++ WebServer/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebServer.Controllers {
+    [HandleError]
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+    }
+}
