7d294bb9fcd89bb436cca8b89ebeff4579e31c7c
Folvaiter Madalina <madalina.folvaiter@info.uaic.ro>
2011-12-16 18:26:15 +0200

    Mutare clasa in folderul stabilit de Catalin

---
+++ WebServer/Data/DocumentManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace WebServer.Data
+{
+
+    public class DocumentManager : IDocumentManager
+    {
+        public int StoreDocument(System.IO.Stream fileStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.IO.Stream GetDocument(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.IO.Stream GetDocument(int id, string type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
