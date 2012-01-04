e97f8b3ebf2e4d572b9dd3d0be9ad2234a3f3cfa
unknown <Dan@connector.(none)>
2012-01-04 18:07:28 +0200

    Scheletul pt. apelarea celorlaltor module...
    Signed-off-by: unknown <Dan@connector.(none)>

---
+++ WebServer/Data/ClientThread.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ApelareCelelalteModule
+{
+    public class ClientThread
+    {
+        private Stream fisierXML;
+        public ClientThread(Stream fisierXML)
+        {
+            this.fisierXML = fisierXML;
+        }
+        public void TrateazaCerere()
+        {
+            // se va trata cererea baza lui fisierXML
+        }
+    }
+}
